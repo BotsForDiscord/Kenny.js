1192415f0e49d1e2be476f797780005976970e89
Donovan Daniels <dwddaniels262@gmail.com>
2018-09-25 19:24:59 -0500

    Update BaseCommand.js

--- BaseCommand.js
+++ BaseCommand.js
@@ -2,7 +2,7 @@ module.exports=((message)=>{
     if(!message) throw new Error("Message parameter is required.");
     global.args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     global.command = args.shift().toLowerCase();
-	global.staff = message.member.roles.some(t=>[config.roles.staff,config.roles.moderator,config.roles.trialModerator].includes(t.id)) || message.member.hasPermission("ADMINISTRATOR");
+	global.staff = message.member.roles.some(t=>[config.roles.moderator,config.roles.trialModerator].includes(t.id)) || message.member.hasPermission("ADMINISTRATOR");
 	
 	global.cmd = config.commandList.all[command];
 	global.category = cmd.category;
