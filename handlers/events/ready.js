7f73546e36529711b6a9bc5337af38a8e14b878a
Donovan Daniels <dwddaniels262@gmail.com>
2018-10-09 13:02:11 -0500

    Update ready.js

--- handlers/events/ready.js
+++ handlers/events/ready.js
@@ -1,9 +1,9 @@
 module.exports = (async(self)=>{
 	console.log(`Bot has started, with ${self.users.size} users, in ${self.channels.size} channels of ${self.guilds.size} guilds.`); 
   	try {
-		  var botCount=client.guilds.get("374071874222686211").members.filter(t=>t.user.bot).size;
-		  client.user.setActivity(`${botCount} Bots in BFD`,{type:"WATCHING"});
+		  var botCount=self.guilds.get("374071874222686211").members.filter(t=>t.user.bot).size;
+		  self.user.setActivity(`${botCount} Bots in BFD`,{type:"WATCHING"});
 	  }catch(e){
-		  client.user.setActivity(`Bots For Discord`,{type:"WATCHING"});
+		  self.user.setActivity(`Bots For Discord`,{type:"WATCHING"});
 	  }
 });
